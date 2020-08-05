882a2e7a7fb79b84617354221a0a937fb1a7e136
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-08-05 08:25:48 +0100

    initial app setup, libraries, folder structure, files

---
+++ src/containers/BlackJack/BlackJack.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlackJack = () => {
+	return (
+		<div>
+			<h1>BlackJack Game</h1>
+		</div>
+	);
+};
+
+export default BlackJack;
---
+++ src/data/gameData.js
@@ -0,0 +1,317 @@
+export const deck = [
+	{
+		rank: "2",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Playing_card_heart_2.svg/800px-Playing_card_heart_2.svg.png",
+	},
+	{
+		rank: "3",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Playing_card_heart_3.svg/800px-Playing_card_heart_3.svg.png",
+	},
+	{
+		rank: "4",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Playing_card_heart_4.svg/800px-Playing_card_heart_4.svg.png",
+	},
+	{
+		rank: "5",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_heart_5.svg/800px-Playing_card_heart_5.svg.png",
+	},
+	{
+		rank: "6",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Playing_card_heart_6.svg/800px-Playing_card_heart_6.svg.png",
+	},
+	{
+		rank: "7",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Playing_card_heart_7.svg/800px-Playing_card_heart_7.svg.png",
+	},
+	{
+		rank: "8",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_heart_8.svg/800px-Playing_card_heart_8.svg.png",
+	},
+	{
+		rank: "9",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_heart_9.svg/800px-Playing_card_heart_9.svg.png",
+	},
+	{
+		rank: "10",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Playing_card_heart_10.svg/800px-Playing_card_heart_10.svg.png",
+	},
+	{
+		rank: "J",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Playing_card_heart_J.svg/800px-Playing_card_heart_J.svg.png",
+	},
+	{
+		rank: "Q",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Playing_card_heart_Q.svg/800px-Playing_card_heart_Q.svg.png",
+	},
+	{
+		rank: "K",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Playing_card_heart_K.svg/800px-Playing_card_heart_K.svg.png",
+	},
+	{
+		rank: "A",
+		color: "hearts",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Playing_card_heart_A.svg/800px-Playing_card_heart_A.svg.png",
+	},
+
+	{
+		rank: "2",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Playing_card_club_2.svg/800px-Playing_card_club_2.svg.png",
+	},
+	{
+		rank: "3",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Playing_card_club_3.svg/800px-Playing_card_club_3.svg.png",
+	},
+	{
+		rank: "4",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Playing_card_club_4.svg/800px-Playing_card_club_4.svg.png",
+	},
+	{
+		rank: "5",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_club_5.svg/800px-Playing_card_club_5.svg.png",
+	},
+	{
+		rank: "6",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Playing_card_club_6.svg/800px-Playing_card_club_6.svg.png",
+	},
+	{
+		rank: "7",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Playing_card_club_7.svg/800px-Playing_card_club_7.svg.png",
+	},
+	{
+		rank: "8",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Playing_card_club_8.svg/800px-Playing_card_club_8.svg.png",
+	},
+	{
+		rank: "9",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Playing_card_club_9.svg/800px-Playing_card_club_9.svg.png",
+	},
+	{
+		rank: "10",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Playing_card_club_10.svg/800px-Playing_card_club_10.svg.png",
+	},
+	{
+		rank: "J",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Playing_card_club_J.svg/800px-Playing_card_club_J.svg.png",
+	},
+	{
+		rank: "Q",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Playing_card_club_Q.svg/800px-Playing_card_club_Q.svg.png",
+	},
+	{
+		rank: "K",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Playing_card_club_K.svg/800px-Playing_card_club_K.svg.png",
+	},
+	{
+		rank: "A",
+		color: "clubs",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Playing_card_club_A.svg/800px-Playing_card_club_A.svg.png",
+	},
+
+	{
+		rank: "2",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Playing_card_spade_2.svg/800px-Playing_card_spade_2.svg.png",
+	},
+	{
+		rank: "3",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_spade_3.svg/800px-Playing_card_spade_3.svg.png",
+	},
+	{
+		rank: "4",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Playing_card_spade_4.svg/800px-Playing_card_spade_4.svg.png",
+	},
+	{
+		rank: "5",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Playing_card_spade_5.svg/800px-Playing_card_spade_5.svg.png",
+	},
+	{
+		rank: "6",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Playing_card_spade_6.svg/800px-Playing_card_spade_6.svg.png",
+	},
+	{
+		rank: "7",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Playing_card_spade_7.svg/800px-Playing_card_spade_7.svg.png",
+	},
+	{
+		rank: "8",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Playing_card_spade_8.svg/800px-Playing_card_spade_8.svg.png",
+	},
+	{
+		rank: "9",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Playing_card_spade_9.svg/800px-Playing_card_spade_9.svg.png",
+	},
+	{
+		rank: "10",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Playing_card_spade_10.svg/800px-Playing_card_spade_10.svg.png",
+	},
+	{
+		rank: "J",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Playing_card_spade_J.svg/800px-Playing_card_spade_J.svg.png",
+	},
+	{
+		rank: "Q",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Playing_card_spade_Q.svg/800px-Playing_card_spade_Q.svg.png",
+	},
+	{
+		rank: "K",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Playing_card_spade_K.svg/800px-Playing_card_spade_K.svg.png",
+	},
+	{
+		rank: "A",
+		color: "spades",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Playing_card_spade_A.svg/800px-Playing_card_spade_A.svg.png",
+	},
+
+	{
+		rank: "2",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Playing_card_diamond_2.svg/800px-Playing_card_diamond_2.svg.png",
+	},
+	{
+		rank: "3",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Playing_card_diamond_3.svg/800px-Playing_card_diamond_3.svg.png",
+	},
+	{
+		rank: "4",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Playing_card_diamond_4.svg/800px-Playing_card_diamond_4.svg.png",
+	},
+	{
+		rank: "5",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Playing_card_diamond_5.svg/800px-Playing_card_diamond_5.svg.png",
+	},
+	{
+		rank: "6",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Playing_card_diamond_6.svg/800px-Playing_card_diamond_6.svg.png",
+	},
+	{
+		rank: "7",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Playing_card_diamond_7.svg/800px-Playing_card_diamond_7.svg.png",
+	},
+	{
+		rank: "8",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Playing_card_diamond_8.svg/800px-Playing_card_diamond_8.svg.png",
+	},
+	{
+		rank: "9",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Playing_card_diamond_9.svg/800px-Playing_card_diamond_9.svg.png",
+	},
+	{
+		rank: "10",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Playing_card_diamond_10.svg/800px-Playing_card_diamond_10.svg.png",
+	},
+	{
+		rank: "J",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Playing_card_diamond_J.svg/800px-Playing_card_diamond_J.svg.png",
+	},
+	{
+		rank: "Q",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Playing_card_diamond_Q.svg/800px-Playing_card_diamond_Q.svg.png",
+	},
+	{
+		rank: "K",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Playing_card_diamond_K.svg/800px-Playing_card_diamond_K.svg.png",
+	},
+	{
+		rank: "A",
+		color: "diamonds",
+		imgUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Playing_card_diamond_A.svg/800px-Playing_card_diamond_A.svg.png",
+	},
+];
